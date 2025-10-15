685b492c1ffb34c9874cc899f6f8ebb38eb87a96
Majeda02 <benlaghfirimajeda@gmail.com>
2025-10-15 10:10:03 +0100

    first commit

---
+++ src/metier/MetierImpl.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package metier;
+
+import dao.IDao;
+
+/**
+ *
+ * @author lenovo
+ */
+public class MetierImpl implements IMetier {
+
+    private IDao dao;
+
+    public void setDao(IDao dao) {
+        this.dao = dao;
+    }
+
+    @Override
+    public double calcul() {
+        return dao.getValue() * 2;
+    }
+}
